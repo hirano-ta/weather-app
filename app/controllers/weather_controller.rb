3c4bd9d50e583764685092e2651713c51f876ec3
hirano-ta <hirano-ta@ibjapan.jp>
2025-05-21 15:33:43 +0900

    formの実装

--- app/controllers/weather_controller.rb
+++ app/controllers/weather_controller.rb
@@ -1,17 +1,30 @@
 class WeatherController < ApplicationController
   def index
-    service = WeatherService.new('tokyo')
-    response = service.fetch_weather
-    weather_data = response.parsed_response
+    # デフォルト表示（東京）
+    @city = "tokyo"
+    @weather = build_weather_data(@city)
+  end
 
-    @weather = {
-      city_name: weather_data['name'],
-      weather_description: weather_data['weather'][0]['description'],
-      temperature: kelvin_to_celsius(weather_data['main']['temp']).round(1),
-      temperature_min: kelvin_to_celsius(weather_data['main']['temp_min']).round(1),
-      temperature_max: kelvin_to_celsius(weather_data['main']['temp_max']).round(1),
-      humidity: weather_data['main']['humidity'],
-      datetime: Time.at(weather_data['dt']).strftime('%Y-%m-%d')
+  def search
+    @city = params[:q].presence || "tokyo"
+    @weather = build_weather_data(@city)
+    render :index
+  end
+
+  private
+
+  def build_weather_data(city)
+    response = WeatherService.new(city).fetch_weather
+    data = response.parsed_response
+
+    {
+      city_name: data["name"],
+      weather_description: data["weather"][0]["description"],
+      temperature: kelvin_to_celsius(data["main"]["temp"]).round(1),
+      temperature_min: kelvin_to_celsius(data["main"]["temp_min"]).round(1),
+      temperature_max: kelvin_to_celsius(data["main"]["temp_max"]).round(1),
+      humidity: data["main"]["humidity"],
+      datetime: Time.at(data["dt"]).strftime('%Y-%m-%d')
     }
   end
 
