4591d972262f5cc082c96a580f23aa244f9cf5e7
hirano-ta <hirano-ta@ibjapan.jp>
2025-05-22 14:41:59 +0900

    allow render

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -86,4 +86,5 @@ Rails.application.configure do
   #
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
-end
+  config.hosts << "weather-app-c1no.onrender.com"
+end 
